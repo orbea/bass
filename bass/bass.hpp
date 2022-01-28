3f7d4440131638f9729bb4da3cd4ecdf063643ea
orbea <orbea@riseup.net>
2022-01-28 09:42:08 -0800

    nall: Remove nall.hpp

--- bass/bass.hpp
+++ bass/bass.hpp
@@ -1,7 +1,16 @@
+#pragma once
+
 #define Architecture NallArchitecture
-#include <nall/nall.hpp>
+
+#include <nall/directory.hpp>
+#include <nall/dl.hpp>
+#include <nall/hashset.hpp>
+#include <nall/set.hpp>
+#include <nall/terminal.hpp>
+
 using namespace nall;
 using string_vector = vector<string>;
+
 #undef Architecture
 
 #include "core/core.hpp"
