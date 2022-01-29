1a92d580ff053c72f7e5270674c7ea03e614f4a3
orbea <orbea@riseup.net>
2022-01-28 18:53:59 -0800

    src: Move all nall includes to one file

--- src/bass.cpp
+++ src/bass.cpp
@@ -2,6 +2,34 @@
 //license: ISC
 //project started: 2013-09-27
 
+#include <nall/intrinsics.hpp>
+#include <nall/memory.hpp>
+#include <nall/iterator.hpp>
+#include <nall/range.hpp>
+#include <nall/array-view.hpp>
+#include <nall/array-span.hpp>
+#include <nall/array.hpp>
+#include <nall/atoi.hpp>
+#include <nall/bit.hpp>
+#include <nall/primitives.hpp>
+#include <nall/function.hpp>
+#include <nall/maybe.hpp>
+#include <nall/merge-sort.hpp>
+#include <nall/vector.hpp>
+#include <nall/shared-pointer.hpp>
+#include <nall/string.hpp>
+#include <nall/location.hpp>
+#include <nall/arithmetic.hpp>
+#include <nall/inode.hpp>
+#include <nall/hash.hpp>
+#include <nall/file-buffer.hpp>
+#include <nall/file.hpp>
+#include <nall/directory.hpp>
+#include <nall/path.hpp>
+#include <nall/hashset.hpp>
+#include <nall/set.hpp>
+#include <nall/terminal.hpp>
+
 #include "bass.hpp"
 #include "core/core.cpp"
 #include "architecture/table/table.cpp"
